c8ae89637767ff88dd663e1b60496f6401110f7a
zzc_meg <zhangzicheng@megmeet.com>
2025-03-02 22:52:03 +0800

    【IMP】0302，权限控制新增NoAuth

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
@@ -115,6 +115,8 @@ public class SecurityConfig
                     // 静态资源，可匿名访问
                     .antMatchers(HttpMethod.GET, "/", "/*.html", "/**/*.html", "/**/*.css", "/**/*.js", "/profile/**").permitAll()
                     .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/*/api-docs", "/druid/**").permitAll()
+                    //前缀标明NoAuth，可匿名访问
+                    .antMatchers("/NoAuth/**","/noAuth/**").permitAll()
                     // 除上面外的所有请求全部需要鉴权认证
                     .anyRequest().authenticated();
             })
